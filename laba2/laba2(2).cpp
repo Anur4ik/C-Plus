36086e68398239e5ee7174901bca410fdacb9162
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-09-23 12:03:49 +0300

    Update laba2(2).cpp

--- laba2/laba2(2).cpp
+++ laba2/laba2(2).cpp
@@ -1,29 +1,37 @@
 #include<iostream>
 #include<math.h>
-#include <cmath>
-using namespace std;
-void check(double x1,double y1){
-    if (-1 <= x1 && x1 <= 1) {
-        if (-1 <= y1 && y1 <= 1) {
-            if(x1<=0 && y1<=0){ std::cout <<"Координати не входять в ромб ";}
-            else {
-                std::cout <<" Координати входять в ромб ";
-            }
+
+
+void check(double x, double y){
+    if (x >= 0 && x <= 1) {
+        if (y <= 1 - x && y >= -1 + x) {
+            std::cout << "Координати входять в ромб ";
+        } else {
+            std::cout << "Координати не входять в ромб ";
         }
-        else{std::cout <<" Координати не входять в ромб";}
+    } else if (x >= -1 && x <= 0) {
+        if (y <= 1 + x && y >= 0) {
+            std::cout << "Координати входять в ромб ";
+        } else {
+            std::cout << "Координати не входять в ромб ";
+        }
+    } else if (x == 0) {
+        if (y >= -1 && y <= 1) {
+            std::cout << "Координати входять в ромб ";
+        } else {
+            std::cout << "Координати не входять в ромб ";
+        }
     }
-    else {
-        std::cout <<" Координати не входять в ромб  " ;
-    }
-
+    else{ std::cout << "Координати не входять в ромб ";}
 }
-int main(){
 
-    double x1;
-    double y1;
+int main(){
+    double x1, y1;
     std::cout << "Enter x = ";
     std::cin >> x1;
     std::cout << "Enter y = ";
     std::cin >> y1;
-    check(x1,y1);
+    check(x1, y1);
+
+    return 0;
 }
