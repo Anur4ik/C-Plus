e057c78da2e5548cfecb4f71fd7b8724888ba0ac
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-09-24 12:54:24 +0300

    Update laba4(6).cpp

--- laba4/laba4(6).cpp
+++ laba4/laba4(6).cpp
@@ -5,16 +5,16 @@ bool answer;
 int value;
 cout << "Enter a value: ";
 cin >> value;
-answer = (value>=0 && value<10 ? true :  false) ;
+if(value>=0 && value<10 ){answer=true;}else{answer=false;}
 cout << value << " >= 0 and " << value << " < 10 \n";
-cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(\n") << endl;
-answer = (value*2<20 && value/2>-2? true :  false) ;
+cout << (answer ? "THAT'S TRUE :" : "THAT'S NOT TRUE :(\n") << endl;
+if(value*2<20 && value/2>-2 ){answer=true;}else{answer=false;}
 cout << value <<"*2 <20 and "<<value<<"/2 >-2 \n";
 cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(\n") << endl;
-answer = (value-1>1 && value/2<10? true :  false) ;
+if(value-1>1 && value/2<10){answer=true;}else{answer=false;}
 cout << value <<"-1 >1 and "<<value<<"/2 <10 \n";
 cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(\n") << endl;
-answer = (value==111? true :  false) ;
+if(value==111){answer=true;}else{answer=false;}
 cout << value <<"=111 \n";
 cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(\n") << endl;
 return 0;
