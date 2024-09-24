b85606eb71ad0fa8c15a925caa93c27748ac6d63
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-09-24 12:27:20 +0300

    Update laba2.cpp

--- laba2/laba2.cpp
+++ laba2/laba2.cpp
@@ -8,11 +8,11 @@ int main() {
         std::cout << "Enter x = ";
         std::cin >> x;
 
-        if (x == 1) {
-            std::cout << "x <1 " << x << std::endl;
+        if (x <= 1) {
+            std::cout << "x <=1 " << std::endl;
             std::cout << ((6 * pow(x, 2)) / 3) << std::endl;
         }
-        else {
+        else if (x>1){
             std::cout << "x >1 " << x << std::endl;
             std::cout << (5 * pow(x, 2) - 4) / 12 << std::endl;
         }
