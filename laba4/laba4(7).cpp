fc3905ca1974bc258f7d26c57cc56025d6866791
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-09-24 13:04:51 +0300

    Update laba4(7).cpp

--- laba4/laba4(7).cpp
+++ laba4/laba4(7).cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 #include <iomanip>
 int main() {
-    double a = 2.3;
-    double b = 2.3;
-    double c = 2.123456;
-    double d = 2.123456;
-    double e = 2.123456;
-    std::cout << a << std::endl;                        
-    std::cout << std::fixed << std::setprecision(2) << b << std::endl; 
-    std::cout << std::setprecision(6) << c << std::endl;    
-    std::cout << std::fixed << std::setprecision(2) << d << std::endl; 
-    std::cout << static_cast<int>(e) << std::endl;       
+    double a;
+    std::cout << "Enter the first integer:";
+    std::cin >> a;
+    std::cout << a<< std::endl;                        
+    std::cout << std::fixed << std::setprecision(2) << a << std::endl; 
+    std::cout << std::setprecision(6) << a << std::endl;    
+    std::cout << std::fixed << std::setprecision(2) << a << std::endl; 
+    std::cout << (int)a << std::endl;       
 
     return 0;
 }
