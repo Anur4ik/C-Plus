e358a2b319e0586d6b756932e648b444857a0f6a
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-10-01 14:30:12 +0300

    Update laba6.cpp

--- laba6/laba6.cpp
+++ laba6/laba6.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
-#include <string>
-int main(){
-    std::cout<<"Введіть Натуральне число ";
-    std::string number; 
-    std::cin>>number;
-    if(number.length()<=1){
-    std::cout<<"Введено не натуральне число ";
-          return 0;
-    }
-    else{
-      std::swap(number[0],number[number.length()-1]);
-      std::cout<<number;
-    }
 
-    
+#include <cmath>
+int main(){
+    int last=0;
+    int first=0;
+    int k,count;
+    int a=12345;
+
+ int size = 0;
+ int change_number;
+ int number=a;
+    if (number == 0) {
+        size = 1; 
+    } else {
+
+        while (number > 0) {
+            
+            if(size==0){
+                last=a%10;
+            }
+            number /= 10; 
+            size++;
+            
+        }
+            first = a / pow(10, size - 1);
+      
+           std::cout <<last <<"   "<<first<< std::endl; 
+           change_number=a-first*pow(10,size-1)+last*pow(10,size-1);
+           std::cout <<change_number;  
+    }
 }
+    
+       
+
