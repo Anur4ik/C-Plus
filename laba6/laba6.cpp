582f7152c88d747d4b59f79c9ff1e50a3c16b972
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-10-01 14:55:05 +0300

    Update laba6.cpp

--- laba6/laba6.cpp
+++ laba6/laba6.cpp
@@ -5,31 +5,28 @@ int main(){
     int last=0;
     int first=0;
     int k,count;
-    int a=12345;
-
+    int a=1234;
+    last=a%10;
  int size = 0;
- int change_number;
  int number=a;
     if (number == 0) {
         size = 1; 
     } else {
 
         while (number > 0) {
-            
-            if(size==0){
-                last=a%10;
-            }
             number /= 10; 
             size++;
             
         }
-            first = a / pow(10, size - 1);
-      
-           std::cout <<last <<"   "<<first<< std::endl; 
-           change_number=a-first*pow(10,size-1)+last*pow(10,size-1);
-           std::cout <<change_number;  
+        first = a / pow(10, size - 1);
+         std::cout <<last <<"   "<<first<< std::endl; 
+         int without_first_and_last = (a - first * pow(10, size - 1)) / 10; 
+         int change_number = last * pow(10, size - 1) + without_first_and_last * 10 + first;
+         std::cout<<change_number ; 
     }
 }
-    
+
        
 
+
+  
