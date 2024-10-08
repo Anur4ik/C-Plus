4176c354a25e3cbbaf776e23536442f657301660
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-10-08 11:12:30 +0300

    Update laba6(1).cpp

--- laba6/laba6(1).cpp
+++ laba6/laba6(1).cpp
@@ -1,44 +1,36 @@
 #include <iostream>
-#include <string>
-#include <list>
+
+#include <cmath>
+using namespace std;
 int main(){
-    int a,k,count;
-    std::cout<<"Введіть Натуральне число ";
-    std::cin>>a;
- int size = 0;
- int number=a;
-    if (number == 0) {
-        size = 1; 
-    } else {
+    int last=0;
+    int first=0;
+    int size = 0;
+    int a;//125
+    cout<<"Enter number:  ";
+    cin>>a;
+    int number=a;
+
         while (number > 0) {
-            number /= 10;
-            size++;
+            number /= 10; //125/10=12/10/=1
+            size++;//3
+        }
+        if(a>=10){
+         first = a / pow(10, size - 1);//125/100=1
+         last=a%10;//125/10=12.5=5
+         cout <<last <<"   "<<first<< std::endl; 
+         int without_first_and_last = (a - first * pow(10, size - 1)) / 10; //(125-1*100)/10=25/10=2
+         
+         int change_number = last * pow(10, size - 1) + without_first_and_last * 10 + first;//5*100+2*10+1=500+20+1
+         cout<<change_number ; 
+        }
+        else{
+              cout<<a; 
         }
     }
-    std::list<int> myList;
-       for(int i=0;i<size;i++){ 
-       k=a%10;
-       myList.push_front(k);
-       a=a/10;
-       }
-       std::list<int>::iterator it = myList.begin();
-       
-       std::advance(it, size);
-       int firstElement = *it; 
-       std::advance(it,1);
-       int lastElement = *it; 
-       *it = firstElement; 
-       std::advance(it, size-1);
-    
-       *it =lastElement; 
-     
-    
+
+
        
 
-   
-     for (int value : myList) {
-        std::cout << value << " ";
-      
-   }
+
   
-}
