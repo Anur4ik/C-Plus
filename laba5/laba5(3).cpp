7b09a46e5ea0867d5a4f9391088b128d8889a166
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-10-08 10:42:52 +0300

    Update laba5(3).cpp

--- laba5/laba5(3).cpp
+++ laba5/laba5(3).cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 #include<math.h>
 #include <cmath> 
+using namespace std;
 int main() {
     double a, b, h;
     std::cout << "Введіть початок відрізка a: ";
     std::cin >> a;
     std::cout << "Введіть кінець відрізка b: ";
     std::cin >> b;
+    
+ 
     std::cout << "Введіть крок h: ";
     std::cin >> h;  
     std::cout <<"-----------"<< std::endl;
     std::cout <<" x     y  "<< std::endl;
     std::cout <<"-----------"<< std::endl;
-      for (double x = a; x <= b; x += h) {
-         double y = pow(cos(x), 1/2) + sin(x);
+    if(a<b){
+  for (double x = a; x <= b; x += h) {
+       if(cos(x)<0){
+           cout<<"Odz is not satisfied";
+           break;
+       }
     
-        std::cout <<" " <<x<<"   "<< round(y* 100) / 100 << std::endl;
-    }
-}
+         double y =sqrt(cos(x)) + sin(x);
+         std::cout <<" " <<x<<"   "<< round(y* 100) / 100 << std::endl;
+    
+  }  
+}}
