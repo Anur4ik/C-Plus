2ce439e14be310094892cff071ff4ed5ab223ea8
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-10-08 10:43:46 +0300

    Update laba5(1).cpp

--- laba5/laba5(1).cpp
+++ laba5/laba5(1).cpp
@@ -1,24 +1,14 @@
 #include<iostream>
-#include<math.h>
 #include <cmath>
 using namespace std;
 
 int main(){
-    double x1,x2,x3,x4,x5;
-  
-    cout << " (x1) :";
-    cin >> x1;
-    cout <<  cbrt(sin(x1))+abs(5*x1-1.5)<< endl;
-    cout << " (x2) :";
-    cin >> x2;
-     cout <<  cbrt(sin(x2))+abs(5*x2-1.5)<< endl;
-    cout << " (x3) :";
-    cin >> x3;
-     cout <<  cbrt(sin(x3))+abs(5*x3-1.5)<< endl;
-   cout << " (x4) :";
-    cin >> x4;
-     cout <<  cbrt(sin(x4))+abs(5*x4-1.5)<< endl;
-    cout << " (x5) :";
-    cin >> x5;
-     cout <<  cbrt(sin(x5))+abs(5*x5-1.5)<< endl;
+    double x;
+  for(int i=0;i<=5;i++){
+        cout << "x"<<i<<" = ";
+         cin >> x;
+         cout <<  pow(sin(x),5)+abs(5*x-1.5)<< endl;
+  }
+    
+    
 }
