3bdc84aedb290eeb5647ac2fb388b07ffde179d0
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-10-22 11:41:06 +0300

    Update laba7(2.7).cpp

--- laba7/laba7(2.7).cpp
+++ laba7/laba7(2.7).cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main(void) {
 double pi4 = 0.;
@@ -6,7 +7,7 @@ long n;
 cout << "Number of iterations? ";
 cin >> n;
 for (int i = 0; i < n; i++) {
-        pi4 += (i % 2 == 0 ? 1.0 : -1.0) / (2.0 * i + 1.0);
+        pi4 += pow(-1, i) / (2*i+1);
     }
 cout.precision(20);
 cout << "Pi = " << (pi4 * 4.) << endl;
