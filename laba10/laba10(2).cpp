7ee61256cddef261bdf3abb335eb5ad3cb059c94
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-10-23 18:39:59 +0300

    Update laba10(2).cpp

--- laba10/laba10(2).cpp
+++ laba10/laba10(2).cpp
@@ -13,8 +13,8 @@ void printArray(int arr[], int size) {
 
 int main(void) {
     srand(time(0));
-    const int m = 4;  
-    const int n = 3;  
+    const int m = 6;  
+    const int n = 4;  
     int array[m][n];  
   for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
