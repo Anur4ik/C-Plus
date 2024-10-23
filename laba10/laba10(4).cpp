b8f5b8a9c5895d2c432dfef28916df7ea99b3126
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-10-23 18:51:05 +0300

    Add files via upload

---
+++ laba10/laba10(4).cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+     cout << endl;
+}
+
+int main(void) {
+    srand(time(0));
+    const int m = 4;  
+    const int n = 3;  
+    int array[m][n];  
+  for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            array[i][j] = rand() % 100 - 50;
+            std::cout << array[i][j] << " ";
+        }
+        std::cout <<  std::endl;
+    }
+
+    for (int i = 0; i < m; ++i) {
+        int evenCount = 0;
+        for (int j = 0; j < n; ++j) {
+            if (array[i][j] % 2 == 0) {
+                evenCount++;
+            }
+        }
+        std::cout << "Number of even numbers in row " << i + 1 << ": " << evenCount << std::endl;
+    }
+
+    return 0;
+    
+}
