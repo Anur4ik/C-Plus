a49cd5e2bc1f484ca25fea7f43276444af119778
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2024-10-23 17:52:11 +0300

    Update laba9(3).cpp

--- laba9/laba9(3).cpp
+++ laba9/laba9(3).cpp
@@ -1,29 +1,41 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-int main(void) {
-  
-    int arr[] = { 3, -5, 1, -2, 7, -8, 4, 9, -9, 2 };
-    int n = sizeof(arr) / sizeof(arr[0]);  
-    int maxIndex = 0;
-    for (int i = 1; i < n; i++) {
+void initial_array(int arr[], int size){
+    for (int i = 0; i < size; i++) {
+      arr[i] = rand() % 201 - 100;//  генерує випадкове число від 0 до 200 зміщує діапазон на 100, щоб він починався від -100 і закінчувався на 100.
+    }
+}
+void modified_array(int arr[], int size){
+     int minIndex,maxIndex = 0;
+    for (int i = 1; i < size; i++) {
         if (arr[i] > arr[maxIndex]) {
             maxIndex = i;
         }
-    }
-
-    for (int i = 0; i < maxIndex; i++) {
-        if (arr[i] < 0) {
-            arr[i] = 0;
+        if (arr[i] < arr[minIndex]) {
+            minIndex = i;
         }
     }
-
-    cout << "Modified array: ";
-    for (int i = 0; i < n; i++) {
+    int max = arr[maxIndex];
+    arr[maxIndex] = arr[minIndex];
+    arr[minIndex] = max;
+}
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
+     cout << endl;
+}
 
-    return 0;
- 
- 
+int main(void) {
+    srand(time(0));
+    const int size = 10;
+    int arr[size];
+     cout << "Initial array: " << endl;
+    initial_array(arr,size);
+    printArray(arr, size);
+    modified_array(arr,size);
+    cout << "Modified array (max and min swapped): " << endl;
+    printArray(arr, size);
 }
