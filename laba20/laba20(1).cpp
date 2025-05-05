c04ed5391e1ed7fa09474b43f06a98a4f617f197
Anur4ik <144905464+Anur4ik@users.noreply.github.com>
2025-05-05 13:16:17 +0300

    Update laba20(1).cpp

--- laba20/laba20(1).cpp
+++ laba20/laba20(1).cpp
@@ -9,9 +9,9 @@ int age;
 string gender;
 
 };
-void print(Person* person)
+void print(Person person)
 {
-cout << person->name << " is "<< person->age << " years old "<< "gender " << person->gender << endl;
+cout << person.name << " is "<< person.age << " years old "<< "gender " << person.gender << endl;
 }
 
 int main()
@@ -23,16 +23,16 @@ person.name = "Harry";
 person.age = 23;
 person.gender = "Male";
 cout << "Meet " << person.name<<endl ;
-print(&person);
+print(person);
 person1.name = "Anna";
 person1.age = 30;
 person1.gender = "Female";
 cout << "Meet " << person1.name << endl;
-print(&person1);
+print(person1);
 person2.name = "John";
 person2.age = 27;
 person2.gender = "Male";
 cout << "Meet " << person2.name << endl;
-print(&person2);
+print(person2);
 return 0;
 }
